aac47611fa4135b375df14a2ac51282128433bec
Ayushz <ayushpoudel321@gmail.com>
2023-03-23 22:45:37 -0400

    Changes in app.js

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World. This is a test');
 });
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
-
