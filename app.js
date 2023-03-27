59e7ef21175a8965900b296de0d2806d3ff7935e
Ayush Poudel <ayushpoudel321@gmail.com>
2023-03-26 22:40:13 -0400

    Staging (#19)
    * Added docker file and app.js
    
    * Necessary changes
    
    * Made changes to workflow and added a new staging.yml
    
    * Changes in app.js
    
    * Corrected mistakes
    
    * CHanges made
    
    * CHange made to docker build
    
    * CHange made to docker build again adding latest
    
    * changes
    
    * changes made
    
    * changes made
    
    * CHanged production yml for staging branch
    
    * CHanged production yml for staging branch
    
    * few config changes
    
    * Made various changes to production.yml file
    
    * Made various changes to production.yml file
    
    * CHanges in production yaml file
    
    * Changes made in both staging and production yml file
    
    * Deleted production.yml file and added docker compose file
    
    * Staging (#15)
    
    * Added docker file and app.js
    
    * Necessary changes
    
    * Made changes to workflow and added a new staging.yml
    
    * Changes in app.js
    
    * Corrected mistakes
    
    * CHanges made
    
    * CHange made to docker build
    
    * CHange made to docker build again adding latest
    
    * changes
    
    * changes made
    
    * changes made
    
    * CHanged production yml for staging branch
    
    * CHanged production yml for staging branch
    
    * few config changes
    
    * Made various changes to production.yml file
    
    * Made various changes to production.yml file
    
    * CHanges in production yaml file
    
    * Changes made in both staging and production yml file
    
    * CHanges made in dockerfile
    
    * CHanges made in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World. This is atest');
+  res.send('Hello World. This is a test');
 });
 
 app.listen(PORT, HOST, () => {
